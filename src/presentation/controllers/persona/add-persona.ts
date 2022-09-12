39a67e0507c8462189a01c7ed716ebae22a6ee64
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-12 10:21:15 -0300

    refactor: garantindo que o AddPersonaController handel implemente interface correta

--- src/presentation/controllers/persona/add-persona.ts
+++ src/presentation/controllers/persona/add-persona.ts
@@ -1,11 +1,16 @@
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+
 export class AddPersonaController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Nome é inválido')
       }
     }
-    return {}
+    return {
+      statusCode: 444,
+      body: {}
+    }
   }
 }
