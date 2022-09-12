410810a9ac74aab7c5665584e58135cd9290d5f1
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-12 20:38:29 -0300

    feat: garantir retorna 500 se houver erro na validação do Nomevalidator

--- src/presentation/controllers/persona/add-persona.ts
+++ src/presentation/controllers/persona/add-persona.ts
@@ -1,4 +1,5 @@
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { ServerError } from '@/presentation/errors/server-error'
 import { badRequestParam } from '@/presentation/helpers/http-helper'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
@@ -7,10 +8,17 @@ import { NameValidator } from '@/presentation/protocols/name-validator'
 export class AddPersonaController implements Controller {
   constructor (private readonly nameValidator: NameValidator) {}
   handle (httpRequest: HttpRequest): HttpResponse {
-    const isValidName = this.nameValidator.isValid(httpRequest.body.name)
-    if (!isValidName) {
-      const sendError = new MissingParamError('name')
-      return badRequestParam(sendError, 'name')
+    try {
+      const isValidName = this.nameValidator.isValid(httpRequest.body.name)
+      if (!isValidName) {
+        const sendError = new MissingParamError('name')
+        return badRequestParam(sendError, 'name')
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
     return {
       statusCode: 500,
