f923cc71d0c71e6408edb461bcf4b0cc984b16b0
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-13 09:55:04 -0300

    refactor: move sut creation to a factory helper method

--- src/tests/unit/validator/name-validator-adapter.spec.ts
+++ src/tests/unit/validator/name-validator-adapter.spec.ts
@@ -1,28 +1,33 @@
 import { NameValidatorAdapter } from '@/utils/name-validator-adapter'
 
+const makeSut = (): NameValidatorAdapter => {
+  const sut = new NameValidatorAdapter()
+  return sut
+}
+
 describe('NameValidator Adapter', () => {
   test('Deve retornar false se o validator retornar false', () => {
-    const sut = new NameValidatorAdapter()
+    const sut = makeSut()
     const name: string = ''
     const isValid = sut.isValid(name)
     expect(isValid).toBeFalsy()
   })
 
   test('Deve retornar false se nome conter caracteres inválidos', () => {
-    const sut = new NameValidatorAdapter()
+    const sut = makeSut()
     const sanitizeName = ''
     const isValid = sut.isValid(sanitizeName)
     expect(isValid).toBeFalsy()
   })
 
   test('Deve retornar false se nome for menor que 5 caracteres', () => {
-    const sut = new NameValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('_no_')
     expect(isValid).toBeFalsy()
   })
 
   test('Deve validar nome com acentuações', () => {
-    const sut = new NameValidatorAdapter()
+    const sut = makeSut()
     const sanitizeName = 'João de Sá'
     const isValid = sut.isValid(sanitizeName)
     expect(isValid).toBeTruthy()
