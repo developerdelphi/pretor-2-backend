7e790f8ed679a181123e3671bf0a225db054250f
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-15 19:08:11 -0300

    refactor: remove interface inputdata

--- src/domain/entity/address.ts
+++ src/domain/entity/address.ts
@@ -1,5 +1,3 @@
-import { inputAddressData } from '../protocols/address-protocols'
-
 export default class Address {
-  constructor (private readonly inputAddressData: inputAddressData) {}
+  constructor (readonly personaId: number, readonly street: string, readonly number?: string, readonly complement?: string, readonly district?: string, readonly cep?: string, readonly city?: string, readonly uf?: string) {}
 }
