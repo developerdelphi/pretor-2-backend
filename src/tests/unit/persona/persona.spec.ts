9c5864cf2ae66cdd3e3386d276ecbbd6fa1dc701
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-15 19:10:08 -0300

    refactor: move test entity persona to correct folder

--- src/tests/unit/persona/persona.spec.ts
+++ src/tests/unit/persona/persona.spec.ts
@@ -1,13 +1,45 @@
+import Address from '@/domain/entity/address'
 import { Persona } from '@/domain/entity/persona'
+import { inputAddressData } from '@/domain/protocols/address-protocols'
+import InputPersonaData from '@/domain/protocols/persona-protocols'
 
-describe('Cadastro de Persona', () => {
-  it('Deve garantir que recebeu um nome válido', () => {
-    const input = {
-      name: 'valid name',
-      kind: 'F'
-    }
+const makeSut = (inputPersonaData: InputPersonaData): Persona => {
+  const sut = new Persona(inputPersonaData)
+  return sut
+}
+
+describe('Persona Entity', () => {
+  test('Deve tentar cadastrar uma pessoa com um nome inválido', () => {
+    const input = { name: 'no', kind: 'F' }
+    expect(() => makeSut(input)).toThrow(new Error('Invalid name'))
+  })
+
+  test('Deve cadastrar uma pessoa com um nome válido', () => {
+    const input = { name: 'valid name', kind: 'F' }
+    const persona = new Persona(input)
+    expect(persona.name.value).toBe('valid name')
+  })
+
+  test('Deve criar uma pessoa como pessoa física', () => {
+    const input = { name: 'valid name', kind: 'F' }
     const sut = new Persona(input)
+    expect(sut.kind).toEqual('F')
+  })
 
-    expect(sut.name.value).toBe('valid name')
+  test('Deve criar uma pessoa com endereço', () => {
+    const input = { name: 'valid name', kind: 'F' }
+    const sut = new Persona(input)
+    const address: inputAddressData = {
+      personaId: 1,
+      street: 'Rua Principal',
+      number: 'sn',
+      complement: 'Qd. 04, Lt. 12',
+      district: 'Centro',
+      cep: '75100-100',
+      city: 'Sossego',
+      uf: 'GO'
+    }
+    sut.addAddress(new Address(address.personaId, address.street, address.number, address.complement, address.district, address.cep, address.city, address.uf))
+    expect(sut.address).toHaveLength(1)
   })
 })
