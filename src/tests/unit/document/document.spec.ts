11f6bc2c1f0119f9a9ae1794c7ab0ce281ba888f
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-15 20:27:06 -0300

    test: ensure entity Document create a new instance

---
+++ src/tests/unit/document/document.spec.ts
@@ -0,0 +1,16 @@
+import Document from '@/domain/entity/document'
+import { InputDocument } from '@/domain/protocols/document-protocols'
+
+describe('Document Entity', () => {
+  test('Deve criar um Document com valores válidos', () => {
+    const input: InputDocument = {
+      type: 'valid_type',
+      identifier: 'valid_identifier',
+      status: 'valid_status'
+    }
+    const sut = new Document(input.type, input.identifier, input.status)
+    expect(sut).toHaveProperty('type', 'valid_type')
+    expect(sut).toHaveProperty('identifier', 'valid_identifier')
+    expect(sut).toHaveProperty('status', 'valid_status')
+  })
+})
