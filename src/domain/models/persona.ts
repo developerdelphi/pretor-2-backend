5042bcc282b5ab9e3ed9abbc958f05e4f361ba37
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-21 20:49:02 -0300

    feat: implement interface PersonaModel include news attributes

--- src/domain/models/persona.ts
+++ src/domain/models/persona.ts
@@ -1,5 +1,9 @@
+import { Address, IDocument, IPhone } from '@/domain/protocols'
 export interface PersonaModel {
   id: string
   name: string
   kind: string
+  address?: Address
+  document?: IDocument
+  phone?: IPhone
 }
