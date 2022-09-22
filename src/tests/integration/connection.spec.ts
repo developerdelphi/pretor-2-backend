bacc3e434842fdf2b3df6126263466292b0e5a58
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-22 14:00:55 -0300

    test: ensure PgPromiseConnectionAdapter return true

---
+++ src/tests/integration/connection.spec.ts
@@ -0,0 +1,9 @@
+import PgPromiseConnectionAdapter from '@/infra/database/pgpromise-connection-adapter'
+
+describe('Conexão com Banco de Dados', () => {
+  test('Deve criar uma conexão com o banco de dados', async () => {
+    const connection = new PgPromiseConnectionAdapter()
+    const resp = await connection.query('select true', [])
+    expect(resp).toBeTruthy()
+  })
+})
