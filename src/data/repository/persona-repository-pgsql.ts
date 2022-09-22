144dfdd698ca0c9817497d150d598e8e8b2ce97d
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-22 20:18:18 -0300

    refactor: remove class not in use

--- src/data/repository/persona-repository-pgsql.ts
+++
@@ -1,8 +0,0 @@
-import { Persona } from '@/domain/entity/persona'
-import { PersonaRepository } from '@/domain/repository/persona-repository'
-
-export class PersonaRepositoryPostGres implements PersonaRepository {
-  async create (persona: Persona): Promise<void> {
-    return await new Promise(resolve => (resolve()))
-  }
-}
