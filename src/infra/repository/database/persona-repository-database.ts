4465c4f46bdf0b1ef4feff53adcb8ea92ca9faa8
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-22 17:58:44 -0300

    feat: ensure PersonaRepositoryDatabase create a new Persona and return PersonaModel

---
+++ src/infra/repository/database/persona-repository-database.ts
@@ -0,0 +1,18 @@
+import { Persona } from '@/domain/entity/persona'
+import { PersonaModel } from '@/domain/models/persona'
+import { PersonaRepository } from '@/domain/repository/persona-repository'
+import { Connection } from '@/infra/database/connection'
+
+export default class PersonaRepositoryDatabase implements PersonaRepository {
+  constructor (readonly connection: Connection) {}
+
+  async create (persona: Persona): Promise<PersonaModel> {
+    const [personaData] = await this.connection.query('insert into pretor.persona (name, kind) values ($1, $2) returning *', [persona.name, persona.kind])
+    const personaModel: PersonaModel = {
+      persona_id: personaData.persona_id,
+      name: personaData.name,
+      kind: personaData.kind
+    }
+    return personaModel
+  }
+}
