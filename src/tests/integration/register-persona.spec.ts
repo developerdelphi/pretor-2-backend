7033ca536d769c2f34b32975dc421f4b06fb35c2
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-22 17:59:20 -0300

    test: ensure PersonaRepositoryDatabase create a new Persona and return PersonaModel

--- src/tests/integration/register-persona.spec.ts
+++ src/tests/integration/register-persona.spec.ts
@@ -1,5 +1,7 @@
 import { AddPersona } from '@/application/usecases/add-persona'
 import { InputPersonaData } from '@/domain/protocols'
+import PgPromiseConnectionAdapter from '@/infra/database/pgpromise-connection-adapter'
+import PersonaRepositoryDatabase from '@/infra/repository/database/persona-repository-database'
 import PersonaRepositoryMemory from '@/infra/repository/memory/persona-repository-memory'
 
 describe('Registrar uma Pessoa - UseCase', () => {
@@ -30,6 +32,38 @@ describe('Registrar uma Pessoa - UseCase', () => {
 
     }
     const newPersona = await addPersona.execute(input)
-    expect(newPersona).toEqual({ id: 'valid_id', name: input.name, kind: input.kind })
+    expect(newPersona).toEqual({ persona_id: 'valid_id', name: input.name, kind: input.kind })
+  })
+
+  test('Deve registrar uma pessoa no sistema em database', async () => {
+    const connection = new PgPromiseConnectionAdapter()
+    const personaRepository = new PersonaRepositoryDatabase(connection)
+    const addPersona = new AddPersona(personaRepository)
+    const input: InputPersonaData = {
+      name: 'Valid Name',
+      kind: 'F',
+      document: [{
+        type: 'cpf',
+        identifier: '111.111.111-11',
+        status: 'active'
+      }],
+      address: [{
+        street: 'Rua Principal',
+        number: '123',
+        complement: 'Qd. 01, Lt. 02, Casa 03',
+        district: 'Centro',
+        cep: '75000-000',
+        city: 'Anápolis',
+        uf: 'GO'
+      }],
+      phone: [{
+        number: '(62) 99999-8877',
+        status: 'active'
+      }]
+
+    }
+    const newPersona = await addPersona.execute(input)
+    expect(newPersona).toHaveProperty('persona_id')
+    expect(newPersona.persona_id).toBeGreaterThan(0)
   })
 })
