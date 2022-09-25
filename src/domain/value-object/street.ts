a4c4307b0318930887cd4f516c5a1d310aa0e033
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-25 08:08:57 -0300

    feat: ensure Street vo have a static create method and return value

--- src/domain/value-object/street.ts
+++ src/domain/value-object/street.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '@/presentation/errors'
+
 export class Street {
   private readonly street: string
 
@@ -6,7 +8,18 @@ export class Street {
     Object.freeze(this)
   }
 
-  // static create(street: string): Street | InvalidParamError{
-  //   if()
-  // }
+  static create (street: string): InvalidParamError | Street {
+    if (!Street.isValid(street)) return new InvalidParamError('street', 'Logradouro é obrigatório')
+    return new Street(street.trim())
+  }
+
+  static isValid (street: string): boolean {
+    if (!street) return false
+    // if (street.trim().length <= 3) return false
+    return true
+  }
+
+  get value (): string {
+    return this.street
+  }
 }
