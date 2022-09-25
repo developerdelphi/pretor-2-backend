5fdc4abe796680b82833a2eaa9e8b1a67c4e67f4
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-25 09:33:43 -0300

    feat: ensure class Street create use Either Class

--- src/domain/value-object/street.ts
+++ src/domain/value-object/street.ts
@@ -1,4 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
+import { Either, left, right } from '@/shared/either'
 
 export class Street {
   private readonly street: string
@@ -8,14 +9,15 @@ export class Street {
     Object.freeze(this)
   }
 
-  static create (street: string): InvalidParamError | Street {
-    if (!Street.isValid(street)) return new InvalidParamError('street', 'Logradouro é obrigatório')
-    return new Street(street.trim())
+  static create (street: string): Either<InvalidParamError, Street> {
+    street = street.trim()
+    if (!Street.isValid(street)) return left(new InvalidParamError('street', 'Logradouro é obrigatório'))
+    return right(new Street(street))
   }
 
   static isValid (street: string): boolean {
     if (!street) return false
-    // if (street.trim().length <= 3) return false
+    if (street.trim().length <= 3) return false
     return true
   }
 
