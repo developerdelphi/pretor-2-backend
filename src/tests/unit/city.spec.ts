2df11fd1e6a2d9f03fd429fc0b779c94fe8b6ebf
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-26 15:07:45 -0300

    test: ensure class City vo to be created with factory method

---
+++ src/tests/unit/city.spec.ts
@@ -0,0 +1,27 @@
+import { InvalidCityError } from '@/domain/error'
+import { City } from '@/domain/value-object/city'
+
+describe('City Value Object', () => {
+  test('Deve criar uma cidade com valor válido', () => {
+    const city = City.create('cidade')
+    expect(city.value).toBeInstanceOf(City)
+  })
+
+  test('Não Deve criar uma cidade com valor inválido', () => {
+    const cityInput = ''
+    const city = City.create(cityInput)
+    expect(city.value).toBeInstanceOf(InvalidCityError)
+  })
+
+  test('Não Deve criar uma cidade com string menor 2 caracteres', () => {
+    const cityInput = 'n'
+    const city = City.create(cityInput)
+    expect(city.value).toBeInstanceOf(InvalidCityError)
+  })
+
+  test('Não Deve criar uma cidade com string maior 100 caracteres', () => {
+    const cityInput = 'n'.repeat(110)
+    const city = City.create(cityInput)
+    expect(city.value).toBeInstanceOf(InvalidCityError)
+  })
+})
