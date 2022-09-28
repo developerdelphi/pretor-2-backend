e767c0bb5cd3039ccc4de2cbd410f5ab4527a6ce
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-28 11:31:14 -0300

    feat: ensure entity Phone implements method create return Phone or InvalidPhoneError

--- src/domain/entity/phone.ts
+++ src/domain/entity/phone.ts
@@ -1,3 +1,13 @@
+import { Either, left, right } from '@/shared/either'
+import { InvalidNumberPhoneError } from '../error'
+import { InputPhoneData } from '../protocols'
+
 export class Phone {
-  constructor (readonly phoneId: string, readonly number: string, readonly status: string = 'active') {}
+  private constructor (readonly phoneId: string, readonly number: string, readonly status: string = 'active') { }
+
+  static create (phone: InputPhoneData): Either<InvalidNumberPhoneError, Phone> {
+    if (!phone.number) return left(new InvalidNumberPhoneError(phone.number))
+    return right(new Phone('0', phone.number)
+    )
+  }
 }
