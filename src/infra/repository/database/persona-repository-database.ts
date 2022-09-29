5cd27a5ac7bd6ea29bfed7f4df649d2f8c8ce40e
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-29 16:31:52 -0300

    feat: ensure class PersonaRepositoryDatabase insert address and phone

--- src/infra/repository/database/persona-repository-database.ts
+++ src/infra/repository/database/persona-repository-database.ts
@@ -8,7 +8,23 @@ export default class PersonaRepositoryDatabase implements PersonaRepository {
   constructor (readonly connection: Connection) {}
 
   async create (persona: Persona): Promise<Either<InvalidNamePersonaError, Persona>> {
-    await this.connection.query('insert into persona (name, kind) values ($1, $2) returning *', [persona.name, persona.kind])
+    const [personaData] = await this.connection.query('insert into persona (name, kind) values ($1, $2) returning *', [persona.name, persona.kind])
+
+    const personaId = personaData.persona_id
+    persona.personaId = personaId
+
+    if (persona.address.length) {
+      for (const address of persona.address) {
+        await this.connection.query('insert into address (persona_id, street, number, complement, district, cep, city, uf, status, obs) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [personaId, address.street.value, address.number, address.complement, address.district.value, address.cep.value, address.city.value, address.uf.value, 'active', ''])
+      }
+    }
+
+    if (persona.phone.length) {
+      for (const phone of persona.phone) {
+        await this.connection.query('insert into phone (persona_id, number, status) values ($1, $2, $3)', [personaId, phone.number, phone.status])
+      }
+    }
+
     return right(persona)
   }
 }
