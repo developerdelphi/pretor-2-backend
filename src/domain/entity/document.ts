9b5decd4169d6d8abef64d01a67c2ee9cd9ffc3b
Oleomar Buchner <developerdelphi@gmail.com>
2022-09-30 11:18:06 -0300

    refactor: ensure class Document implement method static create

--- src/domain/entity/document.ts
+++ src/domain/entity/document.ts
@@ -1,3 +1,26 @@
+import { Either, right } from '@/shared/either'
+import { InvalidParamError } from '../error'
+import { InputDocumentData } from '../protocols'
+
+type DocumentOrError = Either<InvalidParamError, Document>
 export class Document {
-  constructor (readonly documentId: string, readonly type: string, readonly identifier: string, readonly status: string) {}
+  private readonly _id: string
+  private readonly _type: string
+  private readonly _identifier: string
+  private readonly _status: string
+
+  private constructor (readonly documentId: string, readonly type: string, readonly identifier: string, readonly status: string) {
+    this._id = documentId
+    this._type = type
+    this._identifier = identifier
+    this._status = status
+  }
+
+  static create (input: InputDocumentData): DocumentOrError {
+    return right(new Document('0', input.type, input.identifier, input.status))
+  }
+
+  get id (): string {
+    return this._id
+  }
 }
