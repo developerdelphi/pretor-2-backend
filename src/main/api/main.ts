303f6957f698cea15f5fe14791a2564ec6c472b5
Oleomar Buchner <developerdelphi@gmail.com>
2022-10-03 15:28:37 -0300

    refector: change response method post to json

--- src/main/api/main.ts
+++ src/main/api/main.ts
@@ -1,5 +1,5 @@
 import '../config/module-alias'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { AddPersona } from '@/application/usecases/add-persona'
 import { InputPersonaData } from '@/domain/protocols'
 import DatabaseRepositoyFactory from '@/infra/factory/database-repository-factory'
@@ -9,14 +9,22 @@ app.use(express.json())
 const repositoryFactory = new DatabaseRepositoyFactory()
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.post('/persona', async function (req, res) {
+app.post('/persona', async function (req: Request, res: Response) {
   const addPersona = new AddPersona(repositoryFactory)
   const input: InputPersonaData = {
     name: req.body.name,
     kind: req.body.kind
   }
   const resp = await addPersona.execute(input)
-  res.end(resp.value)
+
+  res.json(resp.value)
+})
+
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.setTimeout(400, () => {
+    res.send(408)
+  })
+  next()
 })
 
 app.listen(3000)
