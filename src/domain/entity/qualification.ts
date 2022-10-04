6f136a71fa145f6dbfba4a2e50c00c99079ef205
Oleomar Buchner <developerdelphi@gmail.com>
2022-10-04 19:56:11 -0300

    refactot: ensure class Qualification return Error when invalid values

--- src/domain/entity/qualification.ts
+++ src/domain/entity/qualification.ts
@@ -1,22 +1,28 @@
 import { Either, left, right } from '@/shared/either'
-import { InvalidSortError } from '../error'
+import { InvalidQualityError, InvalidSortError } from '../error'
 import { InputQualificationData, IQualification } from '../protocols'
+import { Quality } from '../value-object/quality'
 import { Sort } from '../value-object/Sort'
 
 export class Qualification implements IQualification {
   private readonly _id: string
   private readonly _sort: Sort
+  private readonly _quality: Quality
 
-  private constructor (id: string, sort: Sort) {
+  private constructor (id: string, sort: Sort, quality: Quality) {
     this._id = id
     this._sort = sort
+    this._quality = quality
   }
 
   static create (input: InputQualificationData): Either<Error, Qualification> {
     const sortOrError: Either<Error, Sort> = Sort.create(input.sort)
+    const qualityOrError: Either<Error, Quality> = Quality.create(input.quality)
     if (sortOrError.isLeft()) return left(new InvalidSortError(input.sort))
+    if (qualityOrError.isLeft()) return left(new InvalidQualityError(input.quality))
     const sort: Sort = sortOrError.value
-    return right(new Qualification(input.id ?? '', sort))
+    const quality: Quality = qualityOrError.value
+    return right(new Qualification(input.id ?? '', sort, quality))
   }
 
   get id (): string {
@@ -25,5 +31,9 @@ export class Qualification implements IQualification {
 
   get sort (): string {
     return this._sort.value
+  }
+
+  get quality (): string {
+    return this._quality.value
   }
 }
--- src/tests/unit/qualification/qualification.spec.ts
+++ src/tests/unit/qualification/qualification.spec.ts
@@ -1,4 +1,5 @@
 import { Qualification } from '@/domain/entity/qualification'
+import { InvalidQualityError, InvalidSortError } from '@/domain/error'
 import { InputQualificationData } from '@/domain/protocols'
 
 describe('Qualification Entity', () => {
@@ -12,5 +13,26 @@ describe('Qualification Entity', () => {
     expect(sut.value).toBeInstanceOf(Qualification)
     expect(sut.value).toHaveProperty('id')
     expect(sut.value).toHaveProperty('sort', 'nacionalidade')
+    expect(sut.value).toHaveProperty('quality', 'brasileira')
+  })
+
+  test('Deve retornar um InvalidSortError se não informar um tipo de qualificação inválido', () => {
+    const input: InputQualificationData = {
+      id: '0',
+      sort: 'n',
+      quality: 'brasileira'
+    }
+    const sut = Qualification.create(input)
+    expect(sut.value).toBeInstanceOf(InvalidSortError)
+  })
+
+  test('Deve retornar um InvalidQualityError se não informar um valor de qualificação inválido', () => {
+    const input: InputQualificationData = {
+      id: '0',
+      sort: 'nacionalidade',
+      quality: '-'
+    }
+    const sut = Qualification.create(input)
+    expect(sut.value).toBeInstanceOf(InvalidQualityError)
   })
 })
