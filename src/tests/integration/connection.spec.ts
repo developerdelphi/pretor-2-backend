46abaa33e4b34c792f3d5ab88796896d3a445ee8
Oleomar Buchner <developerdelphi@gmail.com>
2022-10-04 10:11:06 -0300

    refactor: ensure Connection a database in localhost

--- src/tests/integration/connection.spec.ts
+++ src/tests/integration/connection.spec.ts
@@ -1,10 +1,44 @@
+import { Connection } from '@/infra/database/connection'
 import PgPromiseConnectionAdapter from '@/infra/database/pgpromise-connection-adapter'
+import pgPromise from 'pg-promise'
+
+const makeSut = (): Connection => {
+  class ConnectionStub implements Connection {
+    pgp: any
+    constructor () {
+      // this.pgp = pgPromise()('postgres//pretor:123@localhost:5434/pretor')
+      this.pgp = pgPromise()({
+        host: 'localhost',
+        database: 'pretor',
+        port: 5434,
+        user: 'pretor',
+        password: '123'
+      })
+    }
+
+    async query (statement: string, params: any[]): Promise<any> {
+      const queryResult = await this.pgp.query(statement, params)
+      return queryResult
+    }
+
+    async one (statement: string, params: any[]): Promise<any> {}
+    async close (): Promise<void> {
+      this.pgp.end()
+    }
+  }
+
+  return new ConnectionStub()
+}
+const connection = PgPromiseConnectionAdapter.getInstance()
 
 describe('Conexão com Banco de Dados', () => {
   test('Deve criar uma conexão com o banco de dados', async () => {
-    const connection = PgPromiseConnectionAdapter.getInstance()
-    const resp = await connection.query('select true', [])
-    await connection.close()
+    const sut = makeSut()
+    const resp = await sut.query('select true', [])
     expect(resp).toBeTruthy()
   })
+})
+
+afterEach(async () => {
+  await connection.close()
 })
