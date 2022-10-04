ba0442439b27f963103c72c8654afa67874e24f5
Oleomar Buchner <developerdelphi@gmail.com>
2022-10-04 20:46:17 -0300

    test: ensure entity Persona add new Address value

--- src/tests/unit/persona/persona.spec.ts
+++ src/tests/unit/persona/persona.spec.ts
@@ -1,7 +1,7 @@
-import { Persona } from '@/domain/entity'
+import { Address, Persona } from '@/domain/entity'
 import { Qualification } from '@/domain/entity/qualification'
 import { InvalidNamePersonaError } from '@/domain/error'
-import { InputPersonaData, InputQualificationData } from '@/domain/protocols'
+import { InputAddressData, InputPersonaData, InputQualificationData } from '@/domain/protocols'
 import { Either } from '@/shared/either'
 
 const makeSut = (inputPersonaData: InputPersonaData): Either<InvalidNamePersonaError, Persona> => {
@@ -45,5 +45,36 @@ describe('Persona Entity', () => {
 
     expect(persona?.qualification[0]).toHaveProperty('sort', 'nacionalidade')
     expect(persona?.qualification[0]).toHaveProperty('quality', 'brasileira')
+  })
+
+  test('Deve adicionar um Endereço', () => {
+    const input: InputPersonaData = { name: 'valid name', kind: 'J' }
+    const sut: Either<Error, Persona> = Persona.create(input)
+    let persona
+    let address
+    if (sut.isRight()) {
+      persona = sut.value
+      const inputAddress: InputAddressData = {
+        street: 'Rua vai e vem',
+        number: '100',
+        complement: 'casa 1',
+        district: 'centro',
+        city: 'Goiânia',
+        uf: 'GO',
+        cep: '74100-100',
+        status: 'active'
+      }
+      address = Address.create(inputAddress)
+      if (address.isRight()) { persona.addAddress(address.value) }
+    }
+
+    expect(persona?.address[0]).toHaveProperty('street', 'Rua vai e vem')
+    expect(persona?.address[0]).toHaveProperty('city', 'Goiânia')
+    expect(persona?.address[0]).toHaveProperty('uf', 'GO')
+    expect(persona?.address[0]).toHaveProperty('number', '100')
+    expect(persona?.address[0]).toHaveProperty('complement', 'casa 1')
+    expect(persona?.address[0]).toHaveProperty('district', 'centro')
+    expect(persona?.address[0]).toHaveProperty('cep', '74100-100')
+    expect(persona?.address[0]).toHaveProperty('status', 'active')
   })
 })
